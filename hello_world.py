2f6440283a6b306ee205a9c94881fbd02e63979c
Chris <cmsl4y3r@gmail.com>
2021-07-03 16:58:23 -0600

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+name="Noelle"
+print("Hello", name)
+print("Hello " + name)
+
+number = 7
+print("Hello", number)
+print("Hello " + str(number)) #converted numer to a string 
+
+favoriteFood1="s ushi"
+favoriteFood2="pizza"
+print("I love to eat {} and {}.".format(favoriteFood1, favoriteFood2))
+print(f"I love to eat {favoriteFood1} and {favoriteFood2}.")
+
+print(f"Trying a new string method make sure that its in the string as a method {favoriteFood1.title()} and {favoriteFood2.title()}")
+print(f" Seeing what split will do. {favoriteFood1.split()}")
